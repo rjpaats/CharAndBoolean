35993f896cae0b1b672af219009da0282a5b9a00
R.J. Paats <r.j.paats@paats.nl>
2018-07-01 18:12:08 +0200

    Initial commit

---
+++ src/net/paats/Main.java
@@ -0,0 +1,21 @@
+package net.paats;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // with of 16 bits (2 bytes)
+        char myChar = '\u00A9';
+        System.out.println("\nUnicode output was: " + myChar);
+
+        // with of 1 bit (Only True or False)
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+        //nieuwwe opgaven
+        System.out.println("\nStart nieuwe Opgaven\n");
+
+        char myRegister ='\u00AE';
+        System.out.println("Unicode 00AE gives de register symbol " + myRegister);
+
+    }
+}
